cadbd3c6e9433d5b9829a4a8326fda819d81b66a
tatiana <tatiana.maidana16@gmail.com>
2021-10-24 21:07:40 -0300

    acomodamos vista4

--- src/vistas/Vista4.js
+++ src/vistas/Vista4.js
@@ -2,25 +2,59 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function vista4() {
-    return (
-        <div class= "espejo" style={{backgroundImage: "url('/assets/imagen/fondo1.jpg')"}}>
+  return (
+    <div
+      class="espejo fondo-vista" 
+      style={{ backgroundImage: "url('/assets/imagen/fondo1.jpg')" }}
+    >
+      <div class="col-i">
 
-        <img class="libro2" widht={500} height={300} src="/assets/imagen/libros2.png"/>
+      <img
+          class="libro2"
+          widht={500}
+          height={300}
+          src="/assets/imagen/libros2.png"
+        />
 
-        <video autoPlay controls width={800} height={600}>
-        <source src="/assets/video/espejo.mp4" type="video/mp4" />
-      </video>
+        <img
+          class="texto1-07"
+          widht={413}
+          height={403}
+          src="/assets/imagen/texto1-07.png"
+        />
 
-      <img class="texto1-07" widht={500} height={300} src="/assets/imagen/texto1-07.png"/>
+        <img
+          class="pileta"
+          widht={500}
+          height={300}
+          src="/assets/imagen/pileta.JPG"
+        />
+        
+      </div>
+      <div class="col-d">
 
 
-      <img class="pileta" widht={500} height={300} src="/assets/imagen/pileta.JPG"/>
 
+      <img
+          class="papel 1"
+          widht={500}
+          height={300}
+          src="/assets/imagen/papel 1.png"
+        />
 
+        <video autoPlay controls width={800} height={600} id="video-vista4">
+          <source src="/assets/video/espejo.mp4" type="video/mp4" />
+        </video>
 
-        </div>
-    );
+        <img
+          class="papel 2"
+          widht={500}
+          height={300}
+          src="/assets/imagen/papel 2.png"
+        />
+      </div>
+    </div>
+  );
 }
-
 
 export default vista4;
